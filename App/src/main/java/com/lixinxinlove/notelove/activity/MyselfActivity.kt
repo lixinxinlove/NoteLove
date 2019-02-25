d79a554bf23d49694c4202999a8bd5061cbcd9b2
lixinxin <895330766@qq.com>
2019-02-25 17:05:53 +0800

    添加退出登录

--- App/src/main/java/com/lixinxinlove/notelove/activity/MyselfActivity.kt
+++ App/src/main/java/com/lixinxinlove/notelove/activity/MyselfActivity.kt
@@ -1,8 +1,16 @@
 package com.lixinxinlove.notelove.activity
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.lixinxinlove.base.activity.BaseActivity
 import com.lixinxinlove.notelove.R
+import com.lixinxinlove.notelove.app.NoteApp
+import com.lixinxinlove.user.data.db.NoteDataBaseHelper
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_myself.*
 
 class MyselfActivity : BaseActivity() {
@@ -12,9 +20,48 @@ class MyselfActivity : BaseActivity() {
 
     override fun listener() {
         toolbar.setNavigationOnClickListener { finish() }
+        mBtnLogout.setOnClickListener {
+            logoutAlertDialog()
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mTvUserName.text = NoteApp.user!!.name
     }
+
+
+    private fun logoutAlertDialog() {
+        AlertDialog.Builder(mContext)
+            .setTitle("提示").setMessage("亲！要退出吗？")
+            .setNegativeButton("取消") { dialog, which -> dialog.dismiss() }
+            .setPositiveButton("确定") { dialog, which -> logout() }
+            .create()
+            .show()
+
+    }
+
+    private fun logout() {
+        NoteDataBaseHelper.getInstance(mContext).appDataBase.userDao().deleteUser(NoteApp.user!!).subscribeOn(
+            Schedulers.io()
+        )
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : SingleObserver<Int> {
+                override fun onSuccess(t: Int) {
+                    NoteApp.isLogin = false
+                    NoteApp.user = null
+                    finish()
+                }
+
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onError(e: Throwable) {
+                    Toast.makeText(mContext, "退出登录失败", Toast.LENGTH_SHORT).show()
+                }
+            })
+    }
+
+
 }
