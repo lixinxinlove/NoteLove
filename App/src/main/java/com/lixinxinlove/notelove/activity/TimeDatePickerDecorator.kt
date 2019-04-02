744b89605bd79ee7aad4a5216c39b3aed78fbfde
lixinxin <895330766@qq.com>
2019-04-02 18:18:51 +0800

    选择日期后设置 背景和文字颜色

--- App/src/main/java/com/lixinxinlove/notelove/activity/TimeDatePickerDecorator.kt
+++ App/src/main/java/com/lixinxinlove/notelove/activity/TimeDatePickerDecorator.kt
@@ -12,23 +12,30 @@ class TimeDatePickerDecorator : CalendarCellDecorator {
         if (cellView.isSelectable) {
             val json = cellView.getTag().toString()
             Log.e("TimeDatePickerDecorator", json)
-            cellView.setBackgroundColor(Color.WHITE)
             if (cellView.isSelected) {
                 cellView.dayOfMonthTextView.setTextColor(Color.RED)
-                if (cellView.rangeState == RangeState.FIRST) {
-
-                } else if (cellView.rangeState == RangeState.LAST) {
-
-                } else if (cellView.rangeState == RangeState.MIDDLE) {
-
-                } else {
-
+                when {
+                    cellView.rangeState == RangeState.FIRST -> {
+                        cellView.dayOfMonthTextView.setTextColor(Color.RED)
+                    }
+                    cellView.rangeState == RangeState.LAST -> {
+                        cellView.dayOfMonthTextView.setTextColor(Color.RED)
+                    }
+                    cellView.rangeState == RangeState.MIDDLE -> {
+                        cellView.dayOfMonthTextView.setTextColor(Color.YELLOW)
+                    }
+                    cellView.rangeState == RangeState.NONE -> {
+                        cellView.dayOfMonthTextView.setTextColor(Color.RED)
+                    }
                 }
             } else {
                 cellView.dayOfMonthTextView.setTextColor(Color.parseColor("#333333"))
             }
         } else {
-            cellView.setBackgroundColor(Color.parseColor("#999999"))
+            if (!cellView.isCurrentMonth) {
+                cellView.dayOfMonthTextView.text = ""
+            }
+            cellView.setBackgroundColor(Color.WHITE)
         }
     }
 }
