2e7593466f3354a1c2043d01e5e26153c1e62b42
lixinxin <895330766@qq.com>
2019-04-02 18:25:39 +0800

    添加注释

--- App/src/main/java/com/lixinxinlove/notelove/activity/TimeDatePickerDecorator.kt
+++ App/src/main/java/com/lixinxinlove/notelove/activity/TimeDatePickerDecorator.kt
@@ -1,7 +1,6 @@
 package com.squareup.timessquare
 
 import android.graphics.Color
-import android.util.Log
 import java.util.*
 
 
@@ -9,22 +8,20 @@ class TimeDatePickerDecorator : CalendarCellDecorator {
 
     override fun decorate(cellView: CalendarCellView, date: Date) {
 
-        if (cellView.isSelectable) {
+        if (cellView.isSelectable) {   //可以选择的日期
             val json = cellView.getTag().toString()
-            Log.e("TimeDatePickerDecorator", json)
-            if (cellView.isSelected) {
-                cellView.dayOfMonthTextView.setTextColor(Color.RED)
+            if (cellView.isSelected) {   //被选择的日期
                 when {
-                    cellView.rangeState == RangeState.FIRST -> {
+                    cellView.rangeState == RangeState.FIRST -> {      //被选择的日期开始
                         cellView.dayOfMonthTextView.setTextColor(Color.RED)
                     }
-                    cellView.rangeState == RangeState.LAST -> {
+                    cellView.rangeState == RangeState.LAST -> {  //被选择的日期结束的位置
                         cellView.dayOfMonthTextView.setTextColor(Color.RED)
                     }
-                    cellView.rangeState == RangeState.MIDDLE -> {
+                    cellView.rangeState == RangeState.MIDDLE -> {  //被选择的日期中间的位置
                         cellView.dayOfMonthTextView.setTextColor(Color.YELLOW)
                     }
-                    cellView.rangeState == RangeState.NONE -> {
+                    cellView.rangeState == RangeState.NONE -> {   ////被选择的日期单选的位置
                         cellView.dayOfMonthTextView.setTextColor(Color.RED)
                     }
                 }
@@ -32,7 +29,7 @@ class TimeDatePickerDecorator : CalendarCellDecorator {
                 cellView.dayOfMonthTextView.setTextColor(Color.parseColor("#333333"))
             }
         } else {
-            if (!cellView.isCurrentMonth) {
+            if (!cellView.isCurrentMonth) {   //日期是本月的
                 cellView.dayOfMonthTextView.text = ""
             }
             cellView.setBackgroundColor(Color.WHITE)
